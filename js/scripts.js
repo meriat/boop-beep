c291c87566baedab1064ddb0310e704424dea981
Meria Thomas <josemeria93@gmail.com>
2018-08-17 10:42:37 -0700

    add check divisibility by 3 function

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,27 @@ var toArray = function(number){
   return array;
 };
 
+var replaceNumber = function(number){
+  var arrayOne = [];
+  var divisibleByArray = [];
+  arrayOne = toArray(number);
+  // divisibleByArray = arrayOne;
+  for(var i=1;i<arrayOne.length;i++){
+    while(arrayOne[i] % 3 === 0){
+      arrayOne.splice(i,1, "I'm sorry, Dave. I'm afraid I can't do that.");
+    }
+  }
+  return arrayOne;
+}
+
+var findOne = function(number){
+
+}
+
 $(document).ready(function(){
   $("#input").submit(function(){
-    var number = parseInt($("#number").val());
-    alert(number);
-    alert(toArray(number));
+    var number = $("#number").val();
+    alert(replaceNumber(number));;
   });
 
 });
