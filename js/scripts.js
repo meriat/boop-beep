028bf766e8850ab3535c8a0c1a6a21bf1850ff82
Meria Thomas <josemeria93@gmail.com>
2018-08-17 11:51:33 -0700

    add event default

--- js/scripts.js
+++ js/scripts.js
@@ -13,24 +13,33 @@ var replaceNumber = function(number){
   // divisibleByArray = arrayOne;
   for(var i=1;i<arrayOne.length;i++){
     while(arrayOne[i] % 3 === 0){
+
       arrayOne.splice(i,1, "I'm sorry, Dave. I'm afraid I can't do that.");
     }
   }
   return arrayOne;
 }
 
-var findOne = function(number){
+var findDigitOne = function(number){
+  var findOne = [];
+  findOne = replaceNumber(number);
 
+  // for(j=0;j<findOne.length;j++){
+  //   if(findOne[j].match(/^[1]+$/)){
+  //     findOne.splice(j,1, "Boop");
+  //   }
+  //
+  // }
+  return findOne;
 }
 
 $(document).ready(function(){
-  $("#input").submit(function(){
+  $("#input").submit(function(event){
+    event.preventDefault();
+
     var number = $("#number").val();
-    alert(replaceNumber(number));;
+    // alert(replaceNumber(number));
+    $("#result").append(" " + findDigitOne(number));
   });
 
 });
-// $(document).ready(function(){
-//   alert("yo");
-//   dasfads;
-// });
